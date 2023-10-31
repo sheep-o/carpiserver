01f21fdc81dd2d179628fb5c8712e9ce7a3962ef
rami <rami.kaldany@pm.me>
2023-10-31 12:04:22 -0400

    hi

---
+++ Makefile
@@ -0,0 +1,20 @@
+CC := g++
+CFLAGS := -Wall -g -Iinclude/ -I/opt/homebrew/Cellar/boost/1.81.0_1/include -I/System/Volumes/Data/opt/homebrew/include/ -std=c++20
+TARGET := server
+
+LIBPATHS :=
+LIBS := 
+
+SRCS := $(wildcard *.cpp)
+OBJS := $(patsubst %.cpp, %.o, $(SRCS))
+
+all: $(TARGET)
+$(TARGET): $(OBJS)
+	$(CC) $(LIBPATHS) $(LIBS) -o $@ $^
+%.o: %.cpp
+	$(CC) $(CFLAGS) -lpthread -c $<
+clean:
+	rm -rf $(TARGET) *.o
+all: run
+run: $(TARGET)
+	./$(TARGET)
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <crow.h>
+
+std::vector<crow::websocket::connection*> connections;
+
+void ws_on_open(crow::websocket::connection& conn) {
+    connections.push_back(&conn);
+    std::cout << "New connection" << std::endl;
+}
+
+void ws_on_close(crow::websocket::connection& conn, const std::string& reason) {
+    connections.erase(std::remove(connections.begin(), connections.end(), &conn), connections.end());
+    std::cout << "Removed connection" << std::endl;
+}
+
+void ws_on_message(crow::websocket::connection& conn, const std::string& data, bool is_binary) {
+    if (is_binary) {
+        std::cout << data << std::endl;
+    }
+}
+
+int main() {
+    crow::SimpleApp app;
+
+    CROW_WEBSOCKET_ROUTE(app, "/")
+        .onopen(ws_on_open)
+        .onclose(ws_on_close)
+        .onmessage(ws_on_message);
+
+    app.port(8000).multithreaded().run();
+}
